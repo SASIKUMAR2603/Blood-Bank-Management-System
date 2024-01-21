0d65da2ab2c2499a3c7bcf2ac9e0cbdc74ab01c5
SASIKUMAR2603 <113512657+SASIKUMAR2603@users.noreply.github.com>
2024-01-22 03:54:22 +0530

    Update BloodBank-Database.sql

--- Database/BloodBank/BloodBank-Database.sql
+++ Database/BloodBank/BloodBank-Database.sql
@@ -17,3 +17,4 @@ CREATE TABLE blood_donor (
 INSERT INTO blood_donor (name, age, blood_Group, mobile, state, district, area, availability, email, password,gender)
 VALUES
 ('Saravanan', 21, 'B+', '6383526900', 'TamilNadu', 'Chennai', 'Thiruvanmiyur', 'No', 'dass001@gmail.com', 'password123','Male');
+SELECT * FROM blood_donor;
